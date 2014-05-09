b2cae6bf3a8b0da89f29387b62005fbd79b26ab6
jgowans <gowans.james@gmail.com>
2014-05-09 16:09:45 +0200

    porting of LCD code to Rev1. The library code can still be tidied up a lot, but it works

--- jgowans-sample-projects/lcd/lcd_STM32F0.c
+++ jgowans-sample-projects/lcd/lcd_STM32F0.c
@@ -2,12 +2,12 @@
 Copyright (C) Samuel Ginsberg 2004
 Porting to STM32F0 dev board by James Gowans, 2014
 LCD lines are as follows:
-LCD_RS = PF7
-LCD_E = PF6
-LCD_D4 = PC15
-LCD_D5 = PC14
-LCD_D6 = PC13
-LCD_D7 = PB10
+LCD_RS = PC14
+LCD_E = PC15
+LCD_D4 = PB8
+LCD_D5 = PB9
+LCD_D6 = PB10
+LCD_D7 = PB11
 */
 
 #include "stm32f0xx.h" /* include peripheral declarations */
@@ -36,15 +36,14 @@ void lcd_init () {
 	unsigned int count;
 	for (count=0;count<200;count++) delay(); //allow the LCD some power up time
 	// set the relevant pins to outputs
-	RCC->AHBENR |= RCC_AHBENR_GPIOFEN; //enable clock
 	RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
 	RCC->AHBENR |= RCC_AHBENR_GPIOBEN; 
-	GPIOF->MODER |= GPIO_MODER_MODER7_0; //set pins to outputs
-	GPIOF->MODER |= GPIO_MODER_MODER6_0;
-	GPIOC->MODER |= GPIO_MODER_MODER13_0;
 	GPIOC->MODER |= GPIO_MODER_MODER14_0;
 	GPIOC->MODER |= GPIO_MODER_MODER15_0;
+	GPIOB->MODER |= GPIO_MODER_MODER8_0;
+	GPIOB->MODER |= GPIO_MODER_MODER9_0;
 	GPIOB->MODER |= GPIO_MODER_MODER10_0;
+	GPIOB->MODER |= GPIO_MODER_MODER11_0;
 	for (count=0;count<200;count++) delay(); //allow the LCD some power up time
 
 	lcd_command (0x33); //power up initialization for the lcd
@@ -64,28 +63,16 @@ void lcd_command (unsigned char command) {
 	0x01: Clear screen
 	0x02: Cursor home
 	0xC0: Line 2*/
-	GPIOF->BSRR |= GPIO_BSRR_BR_7;// pull RS (PF7) low
+	GPIOC->BSRR |= GPIO_BSRR_BR_14;// pull RS (PC14) low
 	// clear data lines
-	GPIOB->BSRR |= GPIO_BSRR_BR_10;
-	GPIOC->BSRR |= GPIO_BSRR_BR_13; 
-	GPIOC->BSRR |= GPIO_BSRR_BR_14;
-	GPIOC->BSRR |= GPIO_BSRR_BR_15;
+	GPIOB->BSRR |= (GPIO_BSRR_BR_8 | GPIO_BSRR_BR_9 | GPIO_BSRR_BR_10 | GPIO_BSRR_BR_11);
 	// upper nibble to data lines
-	if (command&0x80) {GPIOB->BSRR |= GPIO_BSRR_BS_10;} 
-	if (command&0x40) {GPIOC->BSRR |= GPIO_BSRR_BS_13;}
-	if (command&0x20) {GPIOC->BSRR |= GPIO_BSRR_BS_14;}
-	if (command&0x10) {GPIOC->BSRR |= GPIO_BSRR_BS_15;}
+	GPIOB->BSRR |= (command & 0xF0) << 4; // shift from bit[7:0] to bit[11:4]
 	pulse_strobe ();
 	// clear data lines
-	GPIOB->BSRR |= GPIO_BSRR_BR_10;
-	GPIOC->BSRR |= GPIO_BSRR_BR_13; 
-	GPIOC->BSRR |= GPIO_BSRR_BR_14;
-	GPIOC->BSRR |= GPIO_BSRR_BR_15;
+	GPIOB->BSRR |= (GPIO_BSRR_BR_8 | GPIO_BSRR_BR_9 | GPIO_BSRR_BR_10 | GPIO_BSRR_BR_11);
 	// lower nibble to data lines
-	if (command&0x08) {GPIOB->BSRR |= GPIO_BSRR_BS_10;} 
-	if (command&0x04) {GPIOC->BSRR |= GPIO_BSRR_BS_13;}
-	if (command&0x02) {GPIOC->BSRR |= GPIO_BSRR_BS_14;}
-	if (command&0x01) {GPIOC->BSRR |= GPIO_BSRR_BS_15;}
+	GPIOB->BSRR |= (command & 0x0F) << 8; // shift from bit[7:0] to bit[15:8]
 	pulse_strobe ();
 }
 
@@ -96,28 +83,16 @@ void lcd_put (unsigned char character) {
 	//The character to be printed is in the input parameter. For numbers, letters 
 	//and other common characters the ASCII code will produce correct display.
 	//Refer to the Hitachi HD44780 datasheet for full character set information.
-	GPIOF->BSRR |= GPIO_BSRR_BS_7;// pull RS (PF7) high
+	GPIOC->BSRR |= GPIO_BSRR_BS_14;// pull RS (PC14) high
 	// clear data lines
-	GPIOB->BSRR |= GPIO_BSRR_BR_10;
-	GPIOC->BSRR |= GPIO_BSRR_BR_13; 
-	GPIOC->BSRR |= GPIO_BSRR_BR_14;
-	GPIOC->BSRR |= GPIO_BSRR_BR_15;
+	GPIOB->BSRR |= (GPIO_BSRR_BR_8 | GPIO_BSRR_BR_9 | GPIO_BSRR_BR_10 | GPIO_BSRR_BR_11);
 	// upper nibble to data lines
-	if (character&0x80) {GPIOB->BSRR |= GPIO_BSRR_BS_10;} 
-	if (character&0x40) {GPIOC->BSRR |= GPIO_BSRR_BS_13;}
-	if (character&0x20) {GPIOC->BSRR |= GPIO_BSRR_BS_14;} // D5
-	if (character&0x10) {GPIOC->BSRR |= GPIO_BSRR_BS_15;} // D4
+	GPIOB->BSRR |= (character & 0xF0) << 4; // shift from bit[7:0] to bit[11:4]
 	pulse_strobe ();
 	// clear data lines
-	GPIOB->BSRR |= GPIO_BSRR_BR_10;
-	GPIOC->BSRR |= GPIO_BSRR_BR_13; 
-	GPIOC->BSRR |= GPIO_BSRR_BR_14;
-	GPIOC->BSRR |= GPIO_BSRR_BR_15;
+	GPIOB->BSRR |= (GPIO_BSRR_BR_8 | GPIO_BSRR_BR_9 | GPIO_BSRR_BR_10 | GPIO_BSRR_BR_11);
 	// lower nibble to data lines
-	if (character&0x08) {GPIOB->BSRR |= GPIO_BSRR_BS_10;} 
-	if (character&0x04) {GPIOC->BSRR |= GPIO_BSRR_BS_13;}
-	if (character&0x02) {GPIOC->BSRR |= GPIO_BSRR_BS_14;}
-	if (character&0x01) {GPIOC->BSRR |= GPIO_BSRR_BS_15;}
+	GPIOB->BSRR |= (character & 0x0F) << 8; // shift from bit[7:0] to bit[15:8]
 	pulse_strobe ();
 }
 
@@ -135,11 +110,11 @@ void pulse_strobe (void)
 //Pulse the strobe line of the LCD to indicate that data is ready.
 {
 delay ();
-GPIOF->BSRR |= GPIO_BSRR_BS_6;// pull RS (PF7) high
+GPIOC->BSRR |= GPIO_BSRR_BS_15;// pull E (PC15) high
 delay ();
-GPIOF->BSRR |= GPIO_BSRR_BR_6;// pull RS (PF7) low
+GPIOC->BSRR |= GPIO_BSRR_BR_15;// pull E (PC15) low
 delay ();
-GPIOF->BSRR |= GPIO_BSRR_BS_6;// pull RS (PF7) high
+GPIOC->BSRR |= GPIO_BSRR_BS_15;// pull E (PC15) high
 }                     
 
 //============================================================================
--- jgowans-sample-projects/lcd/main.c
+++ jgowans-sample-projects/lcd/main.c
@@ -10,12 +10,16 @@ extern void lcd_command (unsigned char command);
 
 void main(void) {
 	int i =0;
-	lcd_init();
+	lcd_init(); // set up LCD lines and send initialisation commands
 	lcd_command(0x01);  //clear screen
-	lcd_string("Hello Tommy.");
-	GPIOB->MODER |= GPIO_MODER_MODER0_0;
-	GPIOB->BSRR |= GPIO_BSRR_BS_0;
+	lcd_string("Hello World");
+	lcd_command(0xC0); // go to lower line
+	lcd_string("Revision 1");
+	// set LEDs to outputs
+	GPIOB->MODER |= (GPIO_MODER_MODER0_0 | GPIO_MODER_MODER1_0 | GPIO_MODER_MODER2_0 | GPIO_MODER_MODER3_0 | 
+				GPIO_MODER_MODER4_0 | GPIO_MODER_MODER5_0 | GPIO_MODER_MODER6_0 | GPIO_MODER_MODER7_0);
 	for(;;) {
-		i++;
+		for(i=0; i < 65535; i++); // delay for a few hundred milliseconds
+		GPIOB->ODR += 1; // increment the patten on the LEDs
 	}
 }
