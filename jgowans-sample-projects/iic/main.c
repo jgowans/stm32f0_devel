ce4a7e700eeacb1d8a045b3e5828a69915e3eadd
jgowans <gowans.james@gmail.com>
2014-05-12 17:44:45 +0200

    I2C temperatuer sensor reading now working, but needs to be tidied up a lot.

--- jgowans-sample-projects/iic/main.c
+++ jgowans-sample-projects/iic/main.c
@@ -12,15 +12,22 @@ void main(void) {
 	init_leds();
 	init_iic();
 	for(;;) {
-		I2C1->CR2 |= (1 << 16); // set NBYTES to 1?
+		// set addressing mode in ADD10
+		//set slave address in SADD
+		I2C2->CR2 |= TC74ADDR_READ;
+		I2C2->CR2 |= (1 << 16); // set NBYTES to 1?
 		// send start and address with read byte. Goes into TXDR
-		I2C1->CR2 |= I2C_CR2_RD_WRN;
+		// indicate we are going to be doing a read
+		I2C2->CR2 |= I2C_CR2_RD_WRN;
 		// START and STOP done in CR2
-		I2C1->CR2 |= I2C_CR2_START;
+		I2C2->CR2 |= I2C_CR2_START;
 		// wait for ACK (start bit reset)
+		// wait for TC flag
+		while ((I2C2->ISR & I2C_ISR_RXNE) == 0);
 		// clock in a byte
+		GPIOB->ODR = I2C2->RXDR;
 		// STOP condition
-		I2C1->CR2 |= I2C_CR2_STOP;
+		I2C2->CR2 |= I2C_CR2_STOP;
 		// wait for RXNE==1
 		// write to LEDs
 	}
@@ -28,33 +35,28 @@ void main(void) {
 
 void init_iic(void) {
 	// enable clock to port B
-	RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
-	// set SCLK (PB8) to alternate function, open drain, with pull-up
-	GPIOB->MODER |= GPIO_MODER_MODER8_1;
-	GPIOB->OTYPER |= GPIO_OTYPER_OT_8;
-	GPIOB->PUPDR |= GPIO_PUPDR_PUPDR8_0;
-	// set SDA (PB9) to alternate function, open drain with pull-up
-	GPIOB->MODER |= GPIO_MODER_MODER9_1;
-	GPIOB->OTYPER |= GPIO_OTYPER_OT_9;
-	GPIOB->PUPDR |= GPIO_PUPDR_PUPDR9_0;
-	// map PB8 and PB9 to I2C1
-	GPIOB->AFR[1] |= 0x01; // PB8 to AF1
-	GPIOB->AFR[1] |= (0x01 << 4); // PB9 to AF1
+	RCC->AHBENR |= RCC_AHBENR_GPIOFEN;
+	// set SCLK (PF6) to alternate function, open drain
+	GPIOF->MODER |= GPIO_MODER_MODER6_1;
+	GPIOF->OTYPER |= GPIO_OTYPER_OT_6;
+	// set SDA (PF7) to alternate function, open drain
+	GPIOF->MODER |= GPIO_MODER_MODER7_1;
+	GPIOF->OTYPER |= GPIO_OTYPER_OT_7;
+	// PF6 and PF7 only have 1 alternate function, so it's not necessary to map them
 	// set up IIC stuff
 	
-	// enable clock to I2C1
-	RCC->APB1ENR |= RCC_APB1ENR_I2C1EN;
+	// enable clock to I2C2
+	RCC->APB1ENR |= RCC_APB1ENR_I2C2EN;
+	// disable the periperal
+	I2C2->CR1 &= ~I2C_CR1_PE;
 	// configure timing in PRESC, SCLDEL, SDADEL in TIMINGR
-	I2C1->TIMINGR |= (0x13 << 0); // SCLL
-	I2C1->TIMINGR |= (0x0F << 8); // SCLH
-	I2C1->TIMINGR |= (0x02 << 16); // SDADEL
-	I2C1->TIMINGR |= (0x04 << 20); // SCLDEL
-	I2C1->TIMINGR |= (1 << 28);// PRESC
-	// set addressing mode in ADD10
-	//set slave address in SADD
-	I2C1->CR2 |= TC74ADDR_READ;
+	I2C2->TIMINGR |= (0x13 << 0); // SCLL
+	I2C2->TIMINGR |= (0x0F << 8); // SCLH
+	I2C2->TIMINGR |= (0x02 << 16); // SDADEL
+	I2C2->TIMINGR |= (0x04 << 20); // SCLDEL
+	I2C2->TIMINGR |= (1 << 28);// PRESC
 	// enable I2C
-	I2C1->CR1 |= I2C_CR1_PE;
+	I2C2->CR1 |= I2C_CR1_PE;
 }
 
 void init_leds(void) {
