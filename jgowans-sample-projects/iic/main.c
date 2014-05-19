5c5ad61ec5b513a4cbd8a48a7724edd470af7610
James Gowans <gowans.james@gmail.com>
2014-05-19 18:04:16 +0200

    added some initial clock toggling to the IIC in order to resolve an issue of the temperature sensor transmitting data while the master tries to send a start condition

--- jgowans-sample-projects/iic/main.c
+++ jgowans-sample-projects/iic/main.c
@@ -36,29 +36,46 @@ void main(void) {
 }
 
 void init_iic(void) {
-	// enable clock to port B
-	RCC->AHBENR |= RCC_AHBENR_GPIOFEN;
-	// set SCLK (PF6) to alternate function, open drain
-	GPIOF->MODER |= GPIO_MODER_MODER6_1;
-	GPIOF->OTYPER |= GPIO_OTYPER_OT_6;
-	// set SDA (PF7) to alternate function, open drain
-	GPIOF->MODER |= GPIO_MODER_MODER7_1;
-	GPIOF->OTYPER |= GPIO_OTYPER_OT_7;
-	// PF6 and PF7 only have 1 alternate function, so it's not necessary to map them
-	// set up IIC stuff
-	
-	// enable clock to I2C2
-	RCC->APB1ENR |= RCC_APB1ENR_I2C2EN;
-	// disable the periperal
-	I2C2->CR1 &= ~I2C_CR1_PE;
-	// configure timing in PRESC, SCLDEL, SDADEL in TIMINGR
-	I2C2->TIMINGR |= (0x13 << 0); // SCLL
-	I2C2->TIMINGR |= (0x0F << 8); // SCLH
-	I2C2->TIMINGR |= (0x02 << 16); // SDADEL
-	I2C2->TIMINGR |= (0x04 << 20); // SCLDEL
-	I2C2->TIMINGR |= (1 << 28);// PRESC
-	// enable I2C
-	I2C2->CR1 |= I2C_CR1_PE;
+  // enable clock to port B
+  RCC->AHBENR |= RCC_AHBENR_GPIOFEN;
+
+  // there is a risk that the slave is sitting in the middle
+  // of a transfer when we reset the master.
+  // the following block of code simply toggles the clock
+  // line 20 times to ensure that the slave gets a chance to 
+  // clock out its data.
+  // set clock line to open drain, output
+  GPIOF->OTYPER |= GPIO_OTYPER_OT_6; // open drain
+  GPIOF->MODER |= GPIO_MODER_MODER6_0; // GP output
+  for (uint32_t loop_counter = 0; loop_counter < 20; loop_counter++) {
+    for (volatile uint32_t delay = 0; delay < 10; delay++);
+    GPIOF->BSRR |= GPIO_BSRR_BR_6; // set clock low
+    for (volatile uint32_t delay = 0; delay < 10; delay++);
+    GPIOF->BSRR |= GPIO_BSRR_BS_6; // set clock high
+  }
+
+  // set SCLK (PF6) to alternate function, open drain
+  GPIOF->MODER &= ~GPIO_MODER_MODER6; // reset the MODER bits
+  GPIOF->MODER |= GPIO_MODER_MODER6_1;
+  GPIOF->OTYPER |= GPIO_OTYPER_OT_6;
+  // set SDA (PF7) to alternate function, open drain
+  GPIOF->MODER |= GPIO_MODER_MODER7_1;
+  GPIOF->OTYPER |= GPIO_OTYPER_OT_7;
+  // PF6 and PF7 only have 1 alternate function, so it's not necessary to map them
+  // set up IIC stuff
+
+  // enable clock to I2C2
+  RCC->APB1ENR |= RCC_APB1ENR_I2C2EN;
+  // disable the periperal
+  I2C2->CR1 &= ~I2C_CR1_PE;
+  // configure timing in PRESC, SCLDEL, SDADEL in TIMINGR
+  I2C2->TIMINGR |= (0xC7 << 0); // SCLL
+  I2C2->TIMINGR |= (0xC3 << 8); // SCLH
+  I2C2->TIMINGR |= (0x02 << 16); // SDADEL
+  I2C2->TIMINGR |= (0x04 << 20); // SCLDEL
+  I2C2->TIMINGR |= (1 << 28);// PRESC
+  // enable I2C
+  I2C2->CR1 |= I2C_CR1_PE;
 }
 
 void init_leds(void) {
