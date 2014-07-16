58793b66a21a1ee100a56ce3015fdbf9c4ad8d0f
James Gowans <gowans.james@gmail.com>
2014-07-16 21:51:38 +0200

    - initial LED cycling now also runs through patterns on the RG LED - cycling interrupt runs 10x faster - all main.c functiosn static - moved RG LED init and deinit funcions into function - EEPROM checks for valid data immediatly after writing data

--- jgowans-sample-projects/demonstration/interrupts.c
+++ jgowans-sample-projects/demonstration/interrupts.c
@@ -4,22 +4,34 @@
 #include "stm32f0xx.h"
 
 void TIM14_IRQHandler(void) {
-  static uint32_t pwm_value = 0;
-  static uint32_t direction = 0;
+  static int16_t ccr3_value = 0;
+  static int16_t ccr4_value = 0;
+  static int16_t ccr3_to_add = 5;
+  static int16_t ccr4_to_add = 6;
+  static uint8_t interrupt_counter = 0;
+
   
   // ack the interrupt
   TIM14->SR &= ~TIM_SR_UIF;
 
   // set PWM's here dependant on direction
-
-  if (pwm_value == 10) { // max value, start going down
-    GPIOB->ODR = 0xFF;
-    direction = -1; 
-  } 
-  if (pwm_value == 0) { // min value, start going up
-    GPIOB->ODR = 0x00;
-    direction = 1;
+  ccr3_value += ccr3_to_add;
+  if (ccr3_value > 255 || ccr3_value < 0) {
+    ccr3_to_add = ccr3_to_add * -1;
+    ccr3_value += ccr3_to_add;
+  }
+  ccr4_value += ccr4_to_add;
+  if (ccr4_value > 255 || ccr4_value < 0) {
+    ccr4_to_add = ccr4_to_add * -1;
+    ccr4_value += ccr4_to_add;
   }
 
-  pwm_value += direction;
+  TIM2->CCR3 = ccr3_value;
+  TIM2->CCR4 = ccr4_value;
+
+  interrupt_counter += 1;
+  if (interrupt_counter == 100) { // toggle after 100 interrupts
+    GPIOB->ODR ^= 0xFF;
+    interrupt_counter = 0;
+  } 
 }
