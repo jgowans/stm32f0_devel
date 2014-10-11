7cb3dc1c8a0498686ff8dbdda0ec54184f0ea195
James Gowans <gowans.james@gmail.com>
2014-10-11 15:54:22 +0200

    fixed makefile and main return in simple-c

--- jgowans-sample-projects/simple-c/main.c
+++ jgowans-sample-projects/simple-c/main.c
@@ -3,10 +3,11 @@
 #include <stdint.h>
 #include "stm32f0xx.h"
 
-void main(void) {
-	*((uint8_t *)TIM2_BASE) = 0xBE;
+int main(void) {
 	volatile uint32_t my_variale_foobar = 0;
+	*((uint8_t *)TIM2_BASE) = 0xBE;
 	for(;;) {
 		my_variale_foobar++;
 	}
+        return 0;
 }
